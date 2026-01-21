1d98c628b9db99584690327b3e8e88ba215bded8
dimita <dimitriomorales01@gmail.com>
2026-01-21 11:13:50 +0100

    Point d'entrée et composant racine Vue.js

---
+++ frontend/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app" class="min-h-screen flex flex-col">
+    <Navbar />
+    <main :class="['flex-grow', isLandingPage ? '' : 'container mx-auto px-4 py-8']">
+      <router-view />
+    </main>
+    <Footer v-if="!isLandingPage" />
+  </div>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+import { useAuthStore } from '@/stores/auth'
+import Navbar from '@/components/Navbar.vue'
+import Footer from '@/components/Footer.vue'
+
+const route = useRoute()
+const authStore = useAuthStore()
+
+const isLandingPage = computed(() => route.name === 'landing')
+</script>
+
+<style scoped>
+#app {
+  font-family: Inter, system-ui, Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
